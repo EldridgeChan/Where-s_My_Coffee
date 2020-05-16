6382f7cef90be8c8b984eeb1cbaf3fb8c9ec358b
EldridgeChan <eldridgechan202@gmail.com>
2020-05-16 17:57:08 +1000

    Revert "opps"
    This reverts commit fc48dab623fd4c932090b47e2c1a31a43163cdca.

---
+++ Assets/Scripts/InteractionManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionManager : MonoBehaviour
+{
+    public bool isHooked = false;
+    public bool isHookRevoking = true;
+
+    [SerializeField]
+    private GameObject player;
+    public GameObject Player{ get{return player;} }
+
+    [SerializeField]
+    private HingeJoint2D playerJoint;
+    public HingeJoint2D PlayerJoint { get { return playerJoint; } }
+
+    [SerializeField]
+    private GameObject hookPrefabs;
+    public GameObject HookPrefabs { get { return hookPrefabs; } }
+
+    [SerializeField]
+    private GameObject ropePrefabs;
+    public GameObject RopePrefabs { get { return ropePrefabs; } }
+
+    [SerializeField]
+    private InputManager inputMan;
+    public InputManager Inputman { get { return inputMan; } }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void destroyRopes(Collider2D collision)
+    {
+        inputMan.currHook.destroyRope(collision);
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Where-s_My_Coffee
+UTS assignment
