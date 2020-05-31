e69c2523f604d57bc8f846449fa1090855250fdc
EldridgeChan <eldridgechan202@gmail.com>
2020-06-01 05:42:13 +1000

    Level Four

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -37,7 +37,7 @@ public class Movement : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.tag == "Traps")
+        if (!interaction.isWin && collision.tag == "Traps")
         {
             interaction.RespawnScript.characterDie();
         }
